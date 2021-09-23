57b73c56780c67b1f1cfa1dd441008c523eea992
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-09-23 11:25:24 -0700

    added lambda urls

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import NProgress from 'nprogress';
 import { mockData } from './mock-data';
 
 
@@ -62,3 +63,32 @@ export const getAccessToken = async () => {
   }
   return accessToken;
 }
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://57dc5trpeg.execute-api.us-west-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
