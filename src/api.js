92d0eba7a67194795bc293d9d00b735961a620cc
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-09-23 09:53:24 -0700

    updating scenario 3 tests/functionality

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
+import { mockData } from './mock-data';
+
 /*
- * @param {*} events:
- * The following function should be in the “api.js” file.
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
@@ -9,4 +9,8 @@ export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
+};
+
+export const getEvents = async () => {
+  return mockData;
 };
