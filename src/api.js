8fa4bf4d451b1edcc8afcf62d13079539a227e76
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-09-23 11:21:21 -0700

    added nprogress

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { mockData } from './mock-data';
+
 
 /*
  * This function takes an events array, then uses map to create a new array with only locations.
@@ -11,6 +13,52 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-export const getEvents = async () => {
-  return mockData;
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+  return result;
 };
+
+export const getEvents = async () => {
+  NProgress.start();
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+  const token = await getAccessToken();
+  if (token) {
+    removeQuery();
+    const url = 'https://57dc5trpeg.execute-api.us-west-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://57dc5trpeg.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
